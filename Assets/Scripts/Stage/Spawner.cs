82cf70993ba01e05d7d929263476400602bbf7f9
Elisey <92730476+Si1verSpruce@users.noreply.github.com>
2022-11-20 21:05:29 +0400

    added spawner

--- Assets/Scripts/Stage/Spawner.cs
+++ Assets/Scripts/Stage/Spawner.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private const float MinPositionX = -3;
+    private const float MaxPositionX = 3;
+    private const float PositionY = 0;
+    private const float PositionZ = 0;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private float _interval;
+    [SerializeField] private Enemy _unit;
+
+    private float _timeFromSpawn;
+
+    private void Update()
     {
-        
+        if (_timeFromSpawn >= _interval)
+        {
+            Instantiate(_unit, new Vector3(Random.Range(MinPositionX, MaxPositionX), PositionY, PositionZ), transform.rotation, transform);
+
+            _timeFromSpawn = 0;
+        }
+        else
+        {
+            _timeFromSpawn += Time.deltaTime;
+        }
     }
 }
