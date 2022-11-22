38fb4ace3b43b2bf2835dcc1284a0f9c521a2a81
Elisey <92730476+Si1verSpruce@users.noreply.github.com>
2022-11-22 18:24:49 +0400

    enemy step by step destroy
    enemy ragdolls deactivating after delay, then they're fall undeground and then destroy

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,11 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Collider))]
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody))]
 public class Enemy : MonoBehaviour
 {
+    private const float DeactivateRagdollPhysicsDelay = 2;
+    private const float FallUndergroundDelay = 4;
+    private const float DestroyDelay = 5;
+
     [SerializeField] private float _health;
     [SerializeField] private float _moveSpeed;
     [SerializeField] private int _damage;
@@ -16,6 +22,7 @@ public class Enemy : MonoBehaviour
     private Animator _animator;
     private Player _player;
     private Collider _collider;
+    private Rigidbody _mainRigidbody;
 
     public float MoveSpeed => _moveSpeed;
     public int Damage => _damage;
@@ -29,8 +36,9 @@ public class Enemy : MonoBehaviour
     {
         _animator = GetComponent<Animator>();
         _collider = GetComponent<Collider>();
+        _mainRigidbody = GetComponent<Rigidbody>();
 
-        SetKinematic(true);
+        SetRagdollKinematic(true);
     }
 
     public void ApplyDamage(float damage)
@@ -41,7 +49,7 @@ public class Enemy : MonoBehaviour
             Die();
     }
 
-    private void SetKinematic(bool isKinematic)
+    private void SetRagdollKinematic(bool isKinematic)
     {
         foreach (var ragdollRigidbody in _ragdollRigidbodies)
             ragdollRigidbody.isKinematic = isKinematic;
@@ -55,9 +63,42 @@ public class Enemy : MonoBehaviour
         _collider.enabled = false;
         _moveSpeed = 0;
         _animator.enabled = false;
+        SetRagdollKinematic(false);
 
-        SetKinematic(false);
+        StartCoroutine(DoAfterDelay(DeactivateRagdollPhysics, DeactivateRagdollPhysicsDelay));
+        StartCoroutine(DoAfterDelay(FallUnderground, FallUndergroundDelay));
+        StartCoroutine(DoAfterDelay(DestroyThisInstance, DestroyDelay));
 
         _player.AddMoney(_reward);
+    }
+
+    private IEnumerator DoAfterDelay(Action action, float delay)
+    {
+        float time = 0;
+
+        while (time <= delay)
+        {
+            yield return null;
+
+            time += Time.deltaTime;
+        }
+
+        action();
+    }
+
+    private void FallUnderground()
+    {
+        _mainRigidbody.isKinematic = false;
+        _mainRigidbody.useGravity = true;
+    }
+
+    private void DestroyThisInstance()
+    {
+        Destroy(gameObject);
+    }
+
+    private void DeactivateRagdollPhysics()
+    {
+        SetRagdollKinematic(true);
     }
 }
