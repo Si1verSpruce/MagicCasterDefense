4211b193e9716396b66dde4681695b810b086438
Elisey <92730476+Si1verSpruce@users.noreply.github.com>
2022-11-28 17:11:00 +0400

    stage map created

---
+++ Assets/Scripts/UI/Menu/StageButtonPlace.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageButtonPlace : MonoBehaviour
+{
+    [SerializeField] private int _number;
+
+    public int Number => _number;
+}
