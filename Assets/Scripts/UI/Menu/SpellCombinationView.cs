5ecfb1b25ef9948d8336faf530f21b19728eaa31
Elisey <92730476+Si1verSpruce@users.noreply.github.com>
2022-12-01 00:49:56 +0400

    sccene

---
+++ Assets/Scripts/UI/Menu/SpellCombinationView.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpellCombinationView : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _label;
+    [SerializeField] private MagicElement[] _combination;
+    [SerializeField] private Transform _combinationContainer;
+
+    private Spell _spell;
+
+    public void Init(Spell spell)
+    {
+        _spell = spell;
+
+        _label.text = _spell.Label;
+
+        foreach (var element in _spell.GetCombination(_combinationContainer))
+            element.GetComponent<Image>().raycastTarget = false;
+    }
+}
