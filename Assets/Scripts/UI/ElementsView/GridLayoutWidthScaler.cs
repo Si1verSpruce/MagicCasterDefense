1c781f69bedfed18a80da516f08a427a9be8d8c5
Elisey <92730476+Si1verSpruce@users.noreply.github.com>
2023-01-07 14:48:53 +0400

    added resizeable grid layout

--- Assets/Scripts/UI/ElementsView/GridLayoutWidthScaler.cs
+++ Assets/Scripts/UI/ElementsView/GridLayoutWidthScaler.cs
@@ -6,26 +6,19 @@ using UnityEngine.UI;
 [RequireComponent(typeof(GridLayoutGroup))]
 public class GridLayoutWidthScaler : MonoBehaviour
 {
-    [SerializeField] private Vector2 _defaultDisplaySize;
-
     private GridLayoutGroup _layoutGroup;
-    private Vector2 _defaultCellSize;
 
     private void Awake()
     {
         _layoutGroup = GetComponent<GridLayoutGroup>();
-        _defaultCellSize = _layoutGroup.cellSize;
     }
 
-    private void Update()
+    private void OnEnable()
     {
-        Debug.Log(Camera.main.pixelWidth);
-        float spacingsWidth = (_layoutGroup.constraintCount - 1) * _layoutGroup.spacing.x;
-        float defaultRatio = (_defaultDisplaySize.x - spacingsWidth) / _defaultDisplaySize.y;
-        float displayRatio = (float)Camera.main.pixelWidth / Camera.main.pixelHeight;
-        float width = _defaultCellSize.x * displayRatio / defaultRatio;
-
-        if (displayRatio < defaultRatio)
-            _layoutGroup.cellSize = new Vector2(width, _layoutGroup.cellSize.y);
+        int columnCount = _layoutGroup.constraintCount;
+        int spacingCount = columnCount - 1;
+        float spacingsWidth = spacingCount * _layoutGroup.spacing.y;
+        float cellWidth = (GetComponent<RectTransform>().rect.width - spacingsWidth) / columnCount;
+        _layoutGroup.cellSize = new Vector2(cellWidth, _layoutGroup.cellSize.y);
     }
 }
