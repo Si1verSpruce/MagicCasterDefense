fbe93a7eb8a34019058a7026d3b069bdde311ce1
Elisey <92730476+Si1verSpruce@users.noreply.github.com>
2023-01-14 04:17:48 +0400

    ReadyBuild

--- Assets/Scripts/Magic/Spells/CastFireBreaths.cs
+++ Assets/Scripts/Magic/Spells/CastFireBreaths.cs
@@ -1,12 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class CastFireBreaths : Spell
 {
     public override void Cast(Instance createdInstance, Vector3 targetPosition)
     {
-        ResetInstance(createdInstance, Vector3.zero, Quaternion.identity);
+        ResetInstance(createdInstance, targetPosition, Quaternion.identity);
     }
 }
